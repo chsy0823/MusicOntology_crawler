73d88fb8854e43f8efb323085efa2ba4e12af9d5
Elenore-iMac <chsy0823@gmail.com>
2016-08-30 03:17:58 +0900

    musicbrainz api test ok

---
+++ musicbrainz_api/build/CMakeFiles/musicbrainz_api.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/musicbrainz_api.dir/main.cpp.o"
+  "musicbrainz_api.pdb"
+  "musicbrainz_api"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/musicbrainz_api.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ musicbrainz_api/main.cpp
@@ -0,0 +1,133 @@
+/* --------------------------------------------------------------------------
+
+   libmusicbrainz5 - Client library to access MusicBrainz
+
+   Copyright (C) 2012 Andrew Hawkins
+
+   This file is part of libmusicbrainz5.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   libmusicbrainz5 is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+     $Id$
+
+----------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <sstream>
+
+#include "musicbrainz5/Query.h"
+#include "musicbrainz5/HTTPFetch.h"
+#include "musicbrainz5/ReleaseGroup.h"
+
+/* For further information, see the web service search documentation:
+ *
+ * http://wiki.musicbrainz.org/Text_Search_Syntax
+ */
+
+
+void DoSearch(const std::string& Entity, const std::string Search)
+{
+
+    std::string int_max = std::to_string(INT32_MAX);
+
+    MusicBrainz5::CQuery Query("queryexample-1.0");
+
+    MusicBrainz5::CQuery::tParamMap Params;
+    Params["query"]=Search;
+    Params["limit"]=int_max;
+
+    try
+    {
+        MusicBrainz5::CMetadata Metadata=Query.Query(Entity,"","",Params);
+
+        //std::cout << "First 100 " << Entity << "s matching: " << Search << std::endl << Metadata << std::endl;
+
+        MusicBrainz5::CReleaseGroupList *groupList = Metadata.ReleaseGroupList();
+        std::cout << "Release Group size: " << groupList->Count() << std::endl;
+
+        for(int i =0; i < groupList->Count(); i++) {
+
+            MusicBrainz5::CReleaseGroup *group = static_cast<MusicBrainz5::CReleaseGroup*>(groupList->Item(i));
+            std::cout << "Release Group title: " << group->Title() << std::endl;
+        }
+
+        //std::cout << "Release Group size: " << Metadata.ReleaseGroupList()->size() << std::endl;
+    }
+
+    catch (MusicBrainz5::CConnectionError& Error)
+    {
+        std::cout << "Connection Exception: '" << Error.what() << "'" << std::endl;
+        std::cout << "LastResult: " << Query.LastResult() << std::endl;
+        std::cout << "LastHTTPCode: " << Query.LastHTTPCode() << std::endl;
+        std::cout << "LastErrorMessage: " << Query.LastErrorMessage() << std::endl;
+    }
+
+    catch (MusicBrainz5::CTimeoutError& Error)
+    {
+        std::cout << "Timeout Exception: '" << Error.what() << "'" << std::endl;
+        std::cout << "LastResult: " << Query.LastResult() << std::endl;
+        std::cout << "LastHTTPCode: " << Query.LastHTTPCode() << std::endl;
+        std::cout << "LastErrorMessage: " << Query.LastErrorMessage() << std::endl;
+    }
+
+    catch (MusicBrainz5::CAuthenticationError& Error)
+    {
+        std::cout << "Authentication Exception: '" << Error.what() << "'" << std::endl;
+        std::cout << "LastResult: " << Query.LastResult() << std::endl;
+        std::cout << "LastHTTPCode: " << Query.LastHTTPCode() << std::endl;
+        std::cout << "LastErrorMessage: " << Query.LastErrorMessage() << std::endl;
+    }
+
+    catch (MusicBrainz5::CFetchError& Error)
+    {
+        std::cout << "Fetch Exception: '" << Error.what() << "'" << std::endl;
+        std::cout << "LastResult: " << Query.LastResult() << std::endl;
+        std::cout << "LastHTTPCode: " << Query.LastHTTPCode() << std::endl;
+        std::cout << "LastErrorMessage: " << Query.LastErrorMessage() << std::endl;
+    }
+
+    catch (MusicBrainz5::CRequestError& Error)
+    {
+        std::cout << "Request Exception: '" << Error.what() << "'" << std::endl;
+        std::cout << "LastResult: " << Query.LastResult() << std::endl;
+        std::cout << "LastHTTPCode: " << Query.LastHTTPCode() << std::endl;
+        std::cout << "LastErrorMessage: " << Query.LastErrorMessage() << std::endl;
+    }
+
+    catch (MusicBrainz5::CResourceNotFoundError& Error)
+    {
+        std::cout << "ResourceNotFound Exception: '" << Error.what() << "'" << std::endl;
+        std::cout << "LastResult: " << Query.LastResult() << std::endl;
+        std::cout << "LastHTTPCode: " << Query.LastHTTPCode() << std::endl;
+        std::cout << "LastErrorMessage: " << Query.LastErrorMessage() << std::endl;
+    }
+}
+
+int main(int argc, const char *argv[])
+{
+    argc=argc;
+    argv=argv;
+
+    //Search for all releases by Kate Bush
+
+    DoSearch("release-group","artist:\"Evanescence\"");
+
+    //Search for all releases with 'sensual' in the title
+
+    //DoSearch("release","release:sensual");
+
+    //Search for all artists with 'john' in the name
+
+    //DoSearch("artist","artist:john");
+}
